13d6c8bee5363aefa8d12ff8e46f475e6c259100
Ekaterina Shliakhovetskaja <shkate@gmail.com>
2014-05-07 15:26:51 +0200

    fix url

--- lastChanges/js/config.js
+++ lastChanges/js/config.js
@@ -22,7 +22,8 @@ angular.module('changesApp.config', [])
             'Accept': 'application/json'
           }
         })
-        .constant('config', { baseUrl: '/teamcity?url=',
+        .constant('config', {
+          baseUrl: '/',
           pageUrl: '/guestAuth/app/rest/changes?locator=count:30,start:0',
           sinceUrl: '/guestAuth/app/rest/changes?sinceChange=id:',
           changeUrl: '/guestAuth/app/rest/changes/id:',
